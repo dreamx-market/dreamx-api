74791c1d8ffaf14f810094d108a3d563aab1ad25
Hieu <hieudoan2609@gmail.com>
2019-01-28 05:00:15 +0700

    update

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class CurrenciesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-  	res = get currencies_url, as: :json
+  	get currencies_url, as: :json
     assert_response :success
+    assert_equal json.length, 3
+    assert_equal json[0]
+    assert_equal json[1]
+    assert_equal json[2]
   end
 end
