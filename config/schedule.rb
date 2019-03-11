8530b9433ca95174d803e87cce4117790c66ddf9
Hieu <hieudoan2609@gmail.com>
2019-03-12 04:10:14 +0700

    add cron tasks for chart data aggregation

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,17 @@
 set :output, "#{path}/log/cron.log"
 
-# every 1.minute do
-#   command "cd #{path} && rake chart:aggregate_5_mins_data RAILS_ENV='test'"
-# end
+every 5.minute do
+  rake "chart:aggregate_5_mins_data"
+end
+
+every 15.minute do
+  rake "chart:aggregate_15_mins_data"
+end
+
+every 1.hour do
+  rake "chart:aggregate_hourly_data"
+end
+
+every 1.day do
+  rake "chart:aggregate_daily_data"
+end
