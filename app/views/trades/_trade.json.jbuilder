773e937cccb7b369f572ccd975786e2b73bb7e9e
Hieu <hieudoan2609@gmail.com>
2019-03-15 20:06:01 +0700

    remove unused routes

--- app/views/trades/_trade.json.jbuilder
+++ app/views/trades/_trade.json.jbuilder
@@ -1,2 +1,1 @@
 json.extract! trade, :id, :account_address, :order_hash, :amount, :nonce, :trade_hash, :signature, :uuid, :created_at, :updated_at
-json.url trade_url(trade, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :transfers, defaults: { format: :json }, param: :account_address, only: [:show]
-  resources :chart_data, defaults: { format: :json }, param: :market_symbol
-  resources :tickers, defaults: { format: :json }, param: :market_symbol
-  resources :order_books, defaults: { format: :json }, param: :market_symbol
-  # resources :deposits
-  resources :withdraws, defaults: { format: :json }
-  resources :order_cancels, defaults: { format: :json }
-  resources :trades, defaults: { format: :json }
-  resources :balances, defaults: { format: :json }, param: :account_address
-  resources :orders, defaults: { format: :json }, param: :order_hash
   get 'return_contract_address', :to => 'helpers#return_contract_address', defaults: { format: :json }
   get 'fees', :to => 'helpers#return_fees', defaults: { format: :json }
+  resources :transfers, defaults: { format: :json }, param: :account_address, only: [:show]
+  resources :chart_data, defaults: { format: :json }, param: :market_symbol, only: [:show]
+  resources :tickers, defaults: { format: :json }, param: :market_symbol, only: [:show, :index]
+  resources :order_books, defaults: { format: :json }, param: :market_symbol, only: [:show]
+  # resources :deposits
+  resources :withdraws, defaults: { format: :json }, only: [:create, :show]
+  resources :order_cancels, defaults: { format: :json }, only: [:create, :show]
+  resources :trades, defaults: { format: :json }, only: [:create, :show]
+  resources :balances, defaults: { format: :json }, param: :account_address, only: [:show]
+  resources :orders, defaults: { format: :json }, param: :order_hash, only: [:index, :show, :create]
   resources :markets, defaults: { format: :json }, only: [:index]
-  resources :tokens, defaults: { format: :json }
+  resources :tokens, defaults: { format: :json }, only: [:index]
 end
