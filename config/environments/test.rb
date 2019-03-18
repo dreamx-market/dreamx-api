20c373f42114b1aaedbd2dffce9e43eb0234e318
Hieu <hieudoan2609@gmail.com>
2019-03-19 03:19:53 +0700

    update contract addresses

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,5 +44,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.CONTRACT_ADDRESS = "0x4ef6474f40bf5c9dbc013efaac07c4d0cb17219a"
+  ENV['CONTRACT_ADDRESS'] = '0x8137064a86006670d407c24e191b5a55da5b2889'
 end
