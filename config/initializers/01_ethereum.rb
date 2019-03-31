c49100f931e83dfebb889dde5efde6115099ed23
Hieu <hieudoan2609@gmail.com>
2019-03-31 16:25:24 +0700

    set default account for ethereum singletons

--- config/initializers/01_ethereum.rb
+++ config/initializers/01_ethereum.rb
@@ -1,5 +1,7 @@
+key = Eth::Key.new(priv: ENV['PRIVATE_KEY'].hex)
 Ethereum::Singleton.client = :http
 Ethereum::Singleton.host = ENV['ETHEREUM_HOST']
+Ethereum::Singleton.default_account = key.address
 Eth.configure do |config|
   chain_id = Ethereum::Singleton.instance.net_version["result"].to_i
   config.chain_id = chain_id
