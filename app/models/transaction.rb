6cb9b18fb43c0f5184e904c787872a7af22d9d58
Hieu <hieudoan2609@gmail.com>
2019-03-31 12:05:20 +0700

    add raw virtual attribute for transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,9 +3,27 @@ class Transaction < ApplicationRecord
 
   after_create :assign_nonce
 
-  # def raw
-  #   key = Eth::Key.new(priv: ENV['PRIVATE_KEY'].hex)    
-  # end
+  def raw
+    client = Ethereum::Singleton.instance
+    key = Eth::Key.new(priv: ENV['PRIVATE_KEY'].hex)    
+    gas_price = client.eth_gas_price['result'].hex.to_i
+    gas_limit = client.eth_get_block_by_number("latest", false)["result"]["gasLimit"].hex.to_i
+    contract_address = ENV['CONTRACT_ADDRESS']
+    payload = self.transactable.payload
+    nonce = self.nonce.to_i
+    args = {
+      from: key.address,
+      to: contract_address,
+      value: 0,
+      data: payload,
+      nonce: nonce,
+      gas_limit: gas_limit,
+      gas_price: gas_price
+    }
+    tx = Eth::Tx.new(args)
+    tx.sign key
+    tx.hex
+  end
 
   # def broadcast
     
