b3fd475d1ed3c38a88d54d52dfd538d98eb5e365
Hieu <hieudoan2609@gmail.com>
2019-04-02 07:48:56 +0700

    refactor accounts helper

--- test/jobs/broadcast_transaction_job_test.rb
+++ test/jobs/broadcast_transaction_job_test.rb
@@ -5,8 +5,14 @@ class BroadcastTransactionJobTest < ActiveJob::TestCase
     ENV['POSTPONE_BROADCASTING'] = 'true'
     sync_nonce
     @withdraws = batch_withdraw([
-      { :account_address => '0xe37a4faa73fced0a177da51d8b62d02764f2fc45', :token_address => '0x0000000000000000000000000000000000000000', :amount => 20000000000000000, :nonce => (Time.now.to_i * 1000).to_s }
+      { :account_address => accounts[0], :token_address => '0x0000000000000000000000000000000000000000', :amount => 20000000000000000, :nonce => (Time.now.to_i * 1000).to_s }
     ])
+    @orders = batch_order([
+      { :account_address => accounts[0], :give_token_address => '0x75d417ab3031d592a781e666ee7bfc3381ad33d5', :give_amount => 1000000000000000000, :take_token_address => '0x0000000000000000000000000000000000000000', :take_amount => 1000000000000000000 }
+    ])
+    # @trades = batch_trade([
+    #   { :account_address => @trade.account_address, :order_hash => @orders[0].order_hash, :amount => @trade.amount }
+    # ])
   end
 
   teardown do
@@ -23,7 +29,7 @@ class BroadcastTransactionJobTest < ActiveJob::TestCase
   #   assert_not_nil transaction.transaction_hash
   # end
 
-  # test "broadcast and update a trade transaction" do
-        
-  # end
+  test "broadcast and update a trade transaction" do
+    p @withdraws.length, @orders.length
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,20 +11,24 @@ class ActiveSupport::TestCase
   	JSON.parse(response.body)
   end
 
-  def accounts
+  def private_keys
     {
-      :"0xe37a4faa73fced0a177da51d8b62d02764f2fc45" => 0xd15b17f51f613d0d89c64c7b629ffff7ae9c19e509afc9518dac1650e9812c18,
-      :"0xa77344043e0b0bada9318f41803e07e9dfc57b0b" => 0xf1caff04b5ff349674820a4eb6ee11c459ad3698ca581c8a8e82ee09591b7aa2,
-      :"0xbfd525710ecb49a266337683971bac0d72d746a6" => 0x24b39c598f81d10af245a6b0c1733be41b63ce4d7ea2e694535a2d1c3730c7b9,
-      :"0xfa46ed8f8d3f15e7d820e7246233bbd9450903e3" => 0x481118f6ea0f477469c7040fdb5fda6d9e2b32a5eea79b68256a20498815ba34,
-      :"0x266a483b876c85fb10c1bd0933e3e64e7ce4ecbb" => 0xf67a9579d888aee21c9ecfb7e9bfbb03940f4e20ee34bab2a3e3aa0c3832bff3,
-      :"0x16e86d3935e8922a9b14c722a97552a202575256" => 0xa23c19df2d411241cd5f6fdef64333e43475c7ee1fb626dd9c956284fd8ceca0,
-      :"0x9a94af493513afc3873c5b6eced09874f0a6f751" => 0x6ffbd5ff2ac4762f0d3e06ed3e253441ff9802bd9fab89bcc0996a5fb737e460,
-      :"0xae5e918b65623660701586ca187c9485c03334bb" => 0xdb3e755d28ee954bdff322be697ba57dd797b6aa8e0dd4ef1edc52ae280e79e9,
-      :"0xf06abaa2ff45cd469c2dab6ad9f8848ce12850d1" => 0xb2be1b7f9e3bb42b30d200623a03fc9dbbf840802567073250e3b6fdff6d3f6e,
-      :"0xcc6cfe1a7f27f84309697beeccbc8112a6b7240a" => 0xa936c57c1e46cec1f70a336c32ecc751bc465070da3fe556d526a082542cc177,
-      :"0x76446f63C6B7756257B9c7D56cE7dDe29836c203" => 0x2b615c2e8ab0fa8ec8c711b5c20c7715d5bb823a40398db9df46f994ab5d53e2
+      "0xe37a4faa73fced0a177da51d8b62d02764f2fc45" => 0xd15b17f51f613d0d89c64c7b629ffff7ae9c19e509afc9518dac1650e9812c18,
+      "0xa77344043e0b0bada9318f41803e07e9dfc57b0b" => 0xf1caff04b5ff349674820a4eb6ee11c459ad3698ca581c8a8e82ee09591b7aa2,
+      "0xbfd525710ecb49a266337683971bac0d72d746a6" => 0x24b39c598f81d10af245a6b0c1733be41b63ce4d7ea2e694535a2d1c3730c7b9,
+      "0xfa46ed8f8d3f15e7d820e7246233bbd9450903e3" => 0x481118f6ea0f477469c7040fdb5fda6d9e2b32a5eea79b68256a20498815ba34,
+      "0x266a483b876c85fb10c1bd0933e3e64e7ce4ecbb" => 0xf67a9579d888aee21c9ecfb7e9bfbb03940f4e20ee34bab2a3e3aa0c3832bff3,
+      "0x16e86d3935e8922a9b14c722a97552a202575256" => 0xa23c19df2d411241cd5f6fdef64333e43475c7ee1fb626dd9c956284fd8ceca0,
+      "0x9a94af493513afc3873c5b6eced09874f0a6f751" => 0x6ffbd5ff2ac4762f0d3e06ed3e253441ff9802bd9fab89bcc0996a5fb737e460,
+      "0xae5e918b65623660701586ca187c9485c03334bb" => 0xdb3e755d28ee954bdff322be697ba57dd797b6aa8e0dd4ef1edc52ae280e79e9,
+      "0xf06abaa2ff45cd469c2dab6ad9f8848ce12850d1" => 0xb2be1b7f9e3bb42b30d200623a03fc9dbbf840802567073250e3b6fdff6d3f6e,
+      "0xcc6cfe1a7f27f84309697beeccbc8112a6b7240a" => 0xa936c57c1e46cec1f70a336c32ecc751bc465070da3fe556d526a082542cc177,
+      "0x76446f63C6B7756257B9c7D56cE7dDe29836c203" => 0x2b615c2e8ab0fa8ec8c711b5c20c7715d5bb823a40398db9df46f994ab5d53e2
     }
+  end
+
+  def accounts
+    private_keys.keys
   end
 
   def assert_model(model, records)
@@ -142,7 +146,7 @@ class ActiveSupport::TestCase
   end
 
   def sign_message(account_address, message)
-    key = Eth::Key.new priv: accounts[:"#{account_address}"]
+    key = Eth::Key.new priv: private_keys[account_address]
     return Eth::Utils.prefix_hex(key.personal_sign(Eth::Utils.hex_to_bin(message)))
   end
 
