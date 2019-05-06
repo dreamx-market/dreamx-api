0bcda2254243b07c4b5bf63b02ed950a8dace4d3
Hieu <hieudoan2609@gmail.com>
2019-05-06 17:41:38 +0700

    check for valid addresses before formatting in Contract::Exchange.deposits

--- lib/contract/exchange.rb
+++ lib/contract/exchange.rb
@@ -31,6 +31,10 @@ module Contract
         block = client.eth_get_block_by_number(i, true)['result']
         if block
           block['transactions'].each do |t|
+            if !Eth::Utils.valid_address?(t['to'])
+              next
+            end
+
             if Eth::Utils.format_address(t['to']) == Eth::Utils.format_address(@instance.address)
               incoming_transactions << t
             end
