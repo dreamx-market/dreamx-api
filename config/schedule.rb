f41c19d38010216dfd0de9c8d39391bda2058c67
Hieu <hieudoan2609@gmail.com>
2019-05-09 02:56:13 +0700

    limit scheduled job instances to one at a time

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,26 +3,25 @@ job_type :rake_with_lock, 'export PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv i
 env :PATH, ENV['PATH'] 
 set :output, "#{path}/log/cron.log"
 
-every 1.minute do
-  rake_with_lock "cron:a_lengthy_task"
+every 5.minutes do
+  rake_with_lock "chart:aggregate_5m"
+  rake_with_lock "block:process_new_confirmed_blocks"
+  rake_with_lock "transaction:broadcast_expired_transactions"
+  rake_with_lock "transaction:confirm_mined_transactions"
 end
 
-# every 5.minutes do
-#   rake "cron:every_5m"
-# end
+every 15.minutes do
+  rake_with_lock "chart:aggregate_15m"
+end
 
-# every 15.minutes do
-#   rake "chart:aggregate_15m"
-# end
+every 1.hour do
+  rake_with_lock "chart:aggregate_hourly"
+end
 
-# every 1.hour do
-#   rake "chart:aggregate_hourly"
-# end
+every 1.day do
+  rake_with_lock "chart:aggregate_daily"
+end
 
-# every 1.day do
-#   rake "chart:aggregate_daily"
-# end
-
-# every 7.days do
-#   rake "chart:remove_expired"
-# end
+every 7.days do
+  rake_with_lock "chart:remove_expired"
+end
