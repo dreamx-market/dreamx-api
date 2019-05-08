7718878bb11776fa70862da1d62f4826de69794d
Hieu <hieudoan2609@gmail.com>
2019-05-08 22:15:41 +0700

    update Block.process_block comments

--- app/models/block.rb
+++ app/models/block.rb
@@ -31,7 +31,8 @@ class Block < ApplicationRecord
 
   def self.process_block(block_number)
     # the following methods should be able to be called multiple times 
-    # with the same arguments without causing errors
+    # with the same arguments without causing errors for scenarios where block#1 is reverted
+    # to an earlier block number
     Deposit.aggregate(block_number)
   end
 
