4196c9108763c07eaa6e2d0bce04d677cad0b5de
Hieu <hieudoan2609@gmail.com>
2019-05-28 20:23:42 +0700

    automatically create tickers on market creations

--- test/models/ticker_test.rb
+++ test/models/ticker_test.rb
@@ -8,7 +8,6 @@ class TickerTest < ActiveSupport::TestCase
 
   test "automatically created on market creations" do
     market = Market.create({ :base_token_address => @token_one.address, :quote_token_address => @token_three.address })
-    pp Ticker.find_by({ :market_symbol => market.symbol })
-    # assert_not_nil Ticker.find_by({ :market_symbol => market.symbol })
+    assert_not_nil Ticker.find_by({ :market_symbol => market.symbol })
   end
 end
