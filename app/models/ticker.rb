443e6447ff7a322e1b6222181d6b1dd5f4eea5e7
Hieu <hieudoan2609@gmail.com>
2019-05-29 14:50:01 +0700

    refactor Ticker class methods

--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -1,47 +1,53 @@
 class Ticker < ApplicationRecord
   validates :market_symbol, uniqueness: true
 
-  def self.find_by_market_symbol(symbol)
-    market = Market.find_by({ :symbol => symbol })
+  class << self
+    def find_by_market_symbol(symbol)
+      market = Market.find_by({ :symbol => symbol })
 
-    if (!market)
-      return
+      if (!market)
+        return
+      end
+
+      return build_ticker(market)
     end
 
-    return build_ticker(market)
-  end
+    def find_all_and_paginate(page, per_page)
+      markets = Market.paginate(:page => page, :per_page => per_page)
 
-  def self.find_all_and_paginate(page, per_page)
-    markets = Market.paginate(:page => page, :per_page => per_page)
+      paginated_tickers = {
+        :total_entries => markets.total_entries,
+        :current_page => Integer(markets.current_page),
+        :per_page => markets.per_page,
+        :records => []
+      }
 
-    paginated_tickers = {
-      :total_entries => markets.total_entries,
-      :current_page => Integer(markets.current_page),
-      :per_page => markets.per_page,
-      :records => []
-    }
+      markets.each do |market|
+        paginated_tickers[:records].push(build_ticker(market))
+      end
 
-    markets.each do |market|
-      paginated_tickers[:records].push(build_ticker(market))
+      return paginated_tickers
     end
 
-    return paginated_tickers
+    def build_ticker(market)
+      ticker = {
+        :base_token_address => market.base_token_address,
+        :quote_token_address => market.quote_token_address,
+        :symbol => market.symbol,
+        :last => market.last_price ? market.last_price.to_s : nil,
+        :high => market.high ? market.high.to_s : nil,
+        :low => market.low ? market.low.to_s : nil,
+        :lowest_ask => market.lowest_ask ? market.lowest_ask.to_s : nil,
+        :highest_bid => market.highest_bid ? market.highest_bid.to_s : nil,
+        :percent_change => market.percent_change_24h.to_s,
+        :base_volume => market.volume.to_s,
+        :quote_volume => market.quote_volume.to_s
+      }
+      return ticker
+    end
   end
 
-  def self.build_ticker(market)
-    ticker = {
-      :base_token_address => market.base_token_address,
-      :quote_token_address => market.quote_token_address,
-      :symbol => market.symbol,
-      :last => market.last_price ? market.last_price.to_s : nil,
-      :high => market.high ? market.high.to_s : nil,
-      :low => market.low ? market.low.to_s : nil,
-      :lowest_ask => market.lowest_ask ? market.lowest_ask.to_s : nil,
-      :highest_bid => market.highest_bid ? market.highest_bid.to_s : nil,
-      :percent_change => market.percent_change_24h.to_s,
-      :base_volume => market.volume.to_s,
-      :quote_volume => market.quote_volume.to_s
-    }
-    return ticker
+  def update
+    pp market_data = Ticker.find_by_market_symbol(self.market_symbol)
   end
 end
