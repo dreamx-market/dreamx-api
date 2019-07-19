3faa1e81ed6ddf9aa55a16d0c45a0b73b1a88d03
Hieu <hieudoan2609@gmail.com>
2019-07-19 15:27:56 +0700

    add test.rb

---
+++ test.rb
@@ -0,0 +1,18 @@
+def calculate_take_amount(give_amount, total_give, total_take)
+  take_amount = total_take * give_amount / total_give
+  return take_amount
+end
+
+def calculate_give_amount(take_amount, total_give, total_take)
+  give_amount = total_give * take_amount / total_take
+  return give_amount
+end
+
+total_give = 195738239776775570
+total_take = 59744193591648150
+take_amount = 50000000000000000
+
+calculated_give_amount = calculate_give_amount(take_amount, total_give, total_take)
+calculated_take_amount = calculate_take_amount(calculated_give_amount, total_give, total_take)
+
+pp calculated_give_amount ,calculated_take_amount
