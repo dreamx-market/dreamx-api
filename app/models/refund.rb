2d03016b7a151ec0e48a129c5dbf1db99dddbeb8
Hieu <hieudoan2609@gmail.com>
2019-10-04 19:06:22 +0700

    refund amount cannot be 0 or lower

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -1,9 +1,17 @@
 class Refund < ApplicationRecord
   belongs_to :balance
 
+  validate :amount_cannot_be_zero
+
   before_create :credit_balance
 
   private
+
+    def amount_cannot_be_zero
+      if (self.amount.to_i <= 0)
+        errors.add(:amount, 'cannot be zero or lower than zero')
+      end
+    end
 
     def credit_balance
       self.balance.credit(self.amount)
