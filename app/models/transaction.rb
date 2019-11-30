00ad888120e27166570b0506333412a0043e4886
Hieu <hieudoan2609@gmail.com>
2019-11-30 16:51:20 +0700

    fix get_onchain_nonce

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -9,6 +9,7 @@ class Transaction < ApplicationRecord
 
   class << self
     def get_onchain_nonce
+      client = Ethereum::Singleton.instance
       key = Eth::Key.new(priv: ENV['SERVER_PRIVATE_KEY'].hex)
       last_onchain_nonce = client.get_nonce(key.address) - 1
     end
