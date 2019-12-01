d3cd5251cc9fcb9188ba6ca6efee4026d02eb262
Hieu <hieudoan2609@gmail.com>
2019-12-02 00:09:40 +0700

    remove unnecessary queries

--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -16,13 +16,19 @@ class Ticker < ApplicationRecord
   end
 
   def build_ticker
+    last_price = self.market.last_price
+    high = self.market.high
+    low = self.market.low
+    highest_bid = self.market.highest_bid
+    lowest_ask = self.market.lowest_ask
+
     ticker = {
       :market_symbol => self.market.symbol,
-      :last => self.market.last_price ? self.market.last_price.to_s : nil,
-      :high => self.market.high ? self.market.high.to_s : nil,
-      :low => self.market.low ? self.market.low.to_s : nil,
-      :lowest_ask => self.market.lowest_ask ? self.market.lowest_ask.to_s : nil,
-      :highest_bid => self.market.highest_bid ? self.market.highest_bid.to_s : nil,
+      :last => last_price ? last_price.to_s : nil,
+      :high => high ? high.to_s : nil,
+      :low => low ? low.to_s : nil,
+      :lowest_ask => lowest_ask ? lowest_ask.to_s : nil,
+      :highest_bid => highest_bid ? highest_bid.to_s : nil,
       :percent_change => self.market.percent_change_24h.to_s,
       :base_volume => self.market.volume.to_s,
       :quote_volume => self.market.quote_volume.to_s
