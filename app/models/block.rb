80d51b15b0b370faaa028de81f45b8e76fe37a33
Hieu <hieudoan2609@gmail.com>
2019-12-02 00:27:57 +0700

    remove unnecessary queries

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,13 +2,14 @@ class Block < ApplicationRecord
   def self.process_new_confirmed_blocks
     client = Ethereum::Singleton.instance
     current_block = client.eth_block_number["result"].hex
+    saved_block = self.last
 
     required_confirmations = ENV['TRANSACTION_CONFIRMATIONS'].to_i
     last_confirmed_block_number = current_block - required_confirmations
-    last_processed_block_number = self.last ? self.last.block_number : last_confirmed_block_number
+    last_processed_block_number = saved_block ? saved_block.block_number : last_confirmed_block_number
     last_block_number = last_processed_block_number
 
-    if (current_block < required_confirmations or (self.last and self.last.block_number == last_confirmed_block_number))
+    if (current_block < required_confirmations or (saved_block and saved_block.block_number == last_confirmed_block_number))
       return
     end
 
@@ -18,7 +19,12 @@ class Block < ApplicationRecord
     end
 
     last_block = client.eth_get_block_by_number(last_block_number, false)
-    self.find_or_initialize_by(:id => 1).update!(:block_number => last_block["result"]["number"].hex, :block_hash => last_block["result"]["hash"], :parent_hash => last_block["result"]["parentHash"])
+
+    if (!saved_block)
+      saved_block = Block.create(:id => 1)
+    end
+
+    saved_block.update!(:block_number => last_block["result"]["number"].hex, :block_hash => last_block["result"]["hash"], :parent_hash => last_block["result"]["parentHash"])
   end
 
   def self.process_block(block_number)
