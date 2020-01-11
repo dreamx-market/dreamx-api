b3f9c1670c184caa095dd439e698bda67ef376ad
Hieu <hieudoan2609@gmail.com>
2020-01-11 17:08:57 +0700

    add logging

--- app/models/block.rb
+++ app/models/block.rb
@@ -20,6 +20,7 @@ class Block < ApplicationRecord
   end
 
   def self.process(from, to=from)
+    AppLogger.log("Processing new blocks from #{from} to #{to}...")
     Deposit.aggregate(from, to)
     Transaction.confirm_mined_transactions(from, to)
   end
