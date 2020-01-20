a39a72d6e092857fc8a3619b47e99e5bad2aafcf
Hieu <hieudoan2609@gmail.com>
2020-01-20 22:31:44 +0700

    fix GET /transfers

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -2,15 +2,18 @@ class TransfersController < ApplicationController
   # GET /transfers/1
   def show
     account = Account.find_by(address: params[:account_address])
+    page = params[:page]
+    per_page = params[:per_page]
 
     if account
       from = params[:start]
       to = params[:end]
-      page = params[:page]
-      per_page = params[:per_page]
       @transfers = account.transfers_within_period(from, to, page, per_page)
     else
-      @transfers = { deposits: [], withdraws: [] }
+      @transfers = { 
+        deposits: [].paginate(page: page, per_page: per_page), # transfers#show expects paginated collections
+        withdraws: [].paginate(page: page, per_page: per_page) 
+      }
     end
   end
 end
