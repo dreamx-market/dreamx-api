e545e9287191aeeb9ef5b8fbe342214993b97980
Hieu <hieudoan2609@gmail.com>
2020-01-20 22:21:07 +0700

    fix GET /transfers

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -10,7 +10,7 @@ class TransfersController < ApplicationController
       per_page = params[:per_page]
       @transfers = account.transfers_within_period(from, to, page, per_page)
     else
-      @transfers = []
+      @transfers = { deposits: [], withdraws: [] }
     end
   end
 end
