795ec8cb506e2dea73359cb5e370c3ef69f453d0
Hieu <hieudoan2609@gmail.com>
2020-02-06 03:31:17 +0700

    update logging

--- app/models/block.rb
+++ app/models/block.rb
@@ -4,27 +4,26 @@ class Block < ApplicationRecord
       return
     end
 
+    last_block = Block.find_or_create_by({ id: 1 })
+    client = Ethereum::Singleton.instance
+    required_confirmations = ENV['TRANSACTION_CONFIRMATIONS'].to_i
+    current_block = client.eth_get_block_by_number('latest', false).convert_keys_to_underscore_symbols![:result]
+    current_block_number = current_block[:number].hex
+    last_block_number = last_block.block_number
+    last_confirmed_block_number = current_block_number - required_confirmations
+    last_processed_block_number = last_block ? last_block.block_number : last_confirmed_block_number
+
+    if (current_block_number < required_confirmations || last_block.block_number == last_confirmed_block_number)
+      return
+    end
+
     begin
       ActiveRecord::Base.transaction do
-        last_block = Block.find_or_create_by({ id: 1 })
-        client = Ethereum::Singleton.instance
-        required_confirmations = ENV['TRANSACTION_CONFIRMATIONS'].to_i
-        current_block = client.eth_get_block_by_number('latest', false).convert_keys_to_underscore_symbols![:result]
-        current_block_number = current_block[:number].hex
-        last_block_number = last_block.block_number
-        last_confirmed_block_number = current_block_number - required_confirmations
-        last_processed_block_number = last_block ? last_block.block_number : last_confirmed_block_number
-
-        if (current_block_number < required_confirmations || last_block.block_number == last_confirmed_block_number)
-          return
-        end
-
         self.process(last_processed_block_number + 1, last_confirmed_block_number)
-
         last_block.update!(block_number: last_confirmed_block_number)
       end
     rescue => err
-      AppLogger.log("Failed to process new blocks, received following error: #{err}")
+      AppLogger.log("Failed to process new blocks from #{last_processed_block_number + 1} to #{last_confirmed_block_number}, received following error: #{err}")
     end
   end
 
